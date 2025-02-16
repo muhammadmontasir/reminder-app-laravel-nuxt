5e73bb73f97514ce9fa2f28ba354d03d5977f903
Montasir Rahman <montasir_rahman@apple.com>
2025-02-16 19:56:23 +0600

    bind scheduler, implement redis, and remove reminder table.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,4 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('events:process-reminders')
+                ->everyMinute()
+                ->withoutOverlapping()
+                ->runInBackground();
+    })
+    ->create();
