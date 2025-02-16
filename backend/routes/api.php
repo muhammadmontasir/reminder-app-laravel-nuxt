c59225b425b43b354e8699ab61295410a128befc
Montasir Rahman <montasir_rahman@apple.com>
2025-02-16 22:57:48 +0600

    apply auth:sanctum to all routes.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,18 +13,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
-});
 
-Route::prefix('v1')->group(function () {
-    Route::get('events', [EventController::class, 'index']);
-    Route::get('events/{eventId}', [EventController::class, 'show']);
-    Route::post('events', [EventController::class, 'store']);
-    Route::put('events/{eventId}', [EventController::class, 'update']);
-    Route::delete('events/{eventId}', [EventController::class, 'destroy']);
-    
-    Route::post('events/sync', [SyncController::class, 'sync'])
-        ->middleware('throttle:60,1');
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('events', EventController::class);
         
-    Route::post('events/import', [ImportController::class, 'import'])
-        ->middleware('throttle:30,1');
+        Route::post('events/sync', [SyncController::class, 'sync'])
+            ->middleware('throttle:60,1');
+            
+        Route::post('events/import', [ImportController::class, 'import'])
+            ->middleware('throttle:30,1');
+    });
 });
